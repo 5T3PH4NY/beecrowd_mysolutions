dee84bcce6ee87db1cc27dfb95f39b4245775431
Stephany <129856514+5T3PH4NY@users.noreply.github.com>
2023-10-14 11:36:40 -0300

    Update Notas_e_moedas_1021.c

--- Iniciante/Notas_e_moedas_1021.c
+++ Iniciante/Notas_e_moedas_1021.c
@@ -1,89 +1,25 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-
+ 
 int main(){
-    
-    float val;
-    int no = 0;
-    
-    scanf("%f", &val);
-    
+    double n, d[] = {100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.25, 0.10, 0.05, 0.01};
+    int t = 0, c;
+    scanf("%lf", &n);
     printf("NOTAS:\n");
-    if(val >= 100.00){
-        no = val/100.00;
-        val = val - (no * 100.00);
-        
-    }printf("%i nota(s) de R$ 100.00\n", no);
-    no = 0;
-    if(val >= 50.00){
-        no = val/50.00;
-        val = val - (no * 50.00);
-        
-    }printf("%i nota(s) de R$ 50.00\n", no);
-    no = 0;
-    if(val >= 20.00){
-        no = val/20.00;
-        val = val - (no * 20.00);
-        
-    }printf("%i nota(s) de R$ 20.00\n", no);
-    no = 0;
-    
-    if(val >= 10.00){
-        no = val/10.00;
-        val = val - (no * 10.00);
-        
-    }printf("%i nota(s) de R$ 10.00\n", no);
-    no = 0;
-    if(val >= 5.00){
-        no = val/5.00;
-        val = val - (no * 5.00);
-        
-    }printf("%i nota(s) de R$ 5.00\n", no);
-    no = 0;
-    if(val >= 2.00){
-        no = val/2.00;
-        val = val - no * 2.00;
-        
-    }printf("%i nota(s) de R$ 2.00\n", no);
-    no = 0;
-    
-    printf("MOEDAS:\n");
-    if(val >= 1.00){
-        no = val/1.00;
-        val = val - no * 1;
-        
-    }printf("%i moeda(s) de R$ 1.00\n", no);
-    no = 0;
-    if(val >= 0.50){
-        no = val/0.50;
-        val = val - (no * 0.50);
-        
-    }printf("%i moeda(s) de R$ 0.50\n", no);
-    no = 0;
-    if(val >= 0.25){
-        no = val/0.25;
-        val = val - (no * 0.25);
-        
-    }printf("%i moeda(s) de R$ 0.25\n", no);
-    no = 0;
-    if(val >= 0.10){
-        no = val/0.10;
-        val = val - (no * 0.10);
-        
-    }printf("%i moeda(s) de R$ 0.10\n", no);
-    no = 0;
-    if(val >= 0.05){
-        no = val/0.05;
-        val = val - (no * 0.05);
-        
-    }printf("%i moeda(s) de R$ 0.05\n", no);
-    no = 0;
-    if(val >= 0.01){
-        no = val/0.01;
-    
-    }printf("%i moeda(s) de R$ 0.01\n", no);
-    
-    
+    t = 0;
+    n+=1e-9;
+    while (d[t] >= 0.01){
+        c = 0;
+        while (n >= d[t]){
+            n -= d[t];
+            c++;
+        }
+        if (d[t] == 1.0)
+            printf("MOEDAS:\n");
+        if (d[t] >= 2.0 )
+            printf("%d nota(s) de R$ %.2f\n", c, d[t]);
+        else
+            printf("%d moeda(s) de R$ %.2f\n", c, d[t]);
+        t++;
+    }
     return 0;
 }
