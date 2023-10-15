7193044eeac73ec1c7ea6fcae52f1592f88342a4
Stephany <129856514+5T3PH4NY@users.noreply.github.com>
2023-10-15 09:35:57 -0300

    Update Carrega_ou_não_carrega_1026.c

--- AD-HOC/Carrega_ou_não_carrega_1026.c
+++ AD-HOC/Carrega_ou_não_carrega_1026.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+/*
+Nesse problema temos um circuito digital
+de soma onde o fio dp carry out não esta
+conectado em lugar nenhum e por isso, al-
+gumas operações não saem como deveriam.
+desse modo: 0 + 0 = 0, 1 + 0 = 1, 0 + 1 =
+1, 1 + 1 = 0 (sem estouro).
+
+colocando essa operação em uma expresão 
+lógica temos:
+(-A v B) ^ (A v -B) a qual pode ser sim-
+plificada em um ou exclusivo. ("^" na
+linguagem c).
+*/
+
 int main(){
     long long int a, b;
 
